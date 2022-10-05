0fafa1bb8cea5f850c1d5b171d663b031fd86927
realdavone <davidkuljovsky@gmail.com>
2022-10-05 19:40:48 +0200

    dist removed from gitignore

---
+++ frontend/dist/assets/CloseButton.0632fdf6.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as _}from"./index.4890fbba.js";const o={};function n(c,s){return t(),_("button",null,"\xD7")}const r=e(o,[["render",n],["__scopeId","data-v-51f33ea5"]]);export{r as default};
