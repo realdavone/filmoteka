31ccad0bdecbc3ed18e729a25136dffb5801a7ac
realdavone <davidkuljovsky@gmail.com>
2022-12-01 20:56:59 +0100

    added onerror handler to network image

--- frontend/src/components/Content/Details.vue
+++ frontend/src/components/Content/Details.vue
@@ -65,7 +65,7 @@
     </div>
 
     <div v-if="details['networks'].length !== 0" class="networks">
-      <img div v-for="network, i in details['networks']" :key="network.id" @error="hideNetwork" :src="`https://www.themoviedb.org/t/p/original${network.logo_path.split('.')[0] + '.svg'}`" :alt="network.name" :title="network.name">
+      <img v-for="network, i in details['networks']" :key="network.id" onerror="javascript:this.remove()" :src="`https://www.themoviedb.org/t/p/original${network.logo_path.split('.')[0]}.svg`" :alt="network.name" :title="network.name">
     </div>
   </section>
 </template>
@@ -83,10 +83,6 @@ const route = useRoute()
 const store = inject('store')
 const type = ref(route.name.charAt(0).toLowerCase() + route.name.substr(1))
 const details = ref(props['details'])
-
-const hideNetwork = el => {
-  el.target.style.display = 'none'
-}
 
 store.methods.recentItems.pushItem({
   type: type.value,
