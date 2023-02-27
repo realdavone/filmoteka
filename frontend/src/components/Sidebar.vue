12bad11201f410640551b23877d75ecd97c0df39
realdavone <davidkuljovsky@gmail.com>
2023-02-27 20:23:28 +0100

    sidebar rework

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -2,16 +2,16 @@
   <aside ref="menu">
     <header class="user-select-none">
       <div class="left-col">
-        <Logo :height="40"/>
+        <button @click="closeMenu" style="font-size: 1.5rem;width:45px;">&times;</button>
       </div>
-      <div class="right-col">
-        <NavButton @handleClick="closeMenu">
-          <template #icon><span style="font-size:1.5rem;font-weight:700;:5px;line-height:1;">&times;</span></template>
-        </NavButton>
+      <div v-if="store.state.credentials.loggedIn" class="right-col">
+        <BasicButton type="transparent" @handleClick="() => {
+          logout()
+          closeMenu()
+        }" title="Odhlásiť">Odhlásiť</BasicButton>
       </div>
     </header>
-    <main class="menu-content">
-      <section v-if="store.state.credentials.loggedIn" class="user-select-none user">
+    <section v-if="store.state.credentials.loggedIn" class="user-select-none user">
         <div class="user-info">
           <div class="username">
             <Avatar :id="store.state.credentials.user._id" />
@@ -19,42 +19,57 @@
             <span v-if="store.state.credentials.user.isVerified" class="material-icons" style="font-size:1rem;color:var(--theme-color)">verified</span>
           </div>
         </div>
-        <BasicButton type="transparent" @handleClick="() => {
-          logout()
-          closeMenu()
-        }" title="Odhlásiť">Odhlásiť</BasicButton>
       </section>
-      <section class="menu-tabs">
-        <SidebarMenuTile v-for="link, i in links" :key="i" :link="link" @click="closeMenu" />
-      </section>
-      <section class="menu-items">
-        <button @click="isVisualMenuOpened = true" class="menu-button">
+    <main class="menu-content">
+      <section class="menu">
+        <router-link @click.native="closeMenu" to="/">
+          <span class="material-icons-outlined">home</span>
+          <span class="label">Domov</span>
+        </router-link>
+        <router-link @click.native="closeMenu" to="/library">
+          <span class="material-icons-outlined">video_library</span>
+          <span class="label">Knižnica</span>
+        </router-link>
+        <router-link @click.native="closeMenu" to="/recommended">
+          <span class="material-icons-outlined">recommend</span>
+          <span class="label">Odporúčane</span>
+        </router-link>
+        <router-link @click.native="closeMenu" v-if="store.state.credentials.user?.isAdmin" to="/admin">
+          <span class="material-icons-outlined">admin_panel_settings</span>
+          <span class="label">Admin</span>
+        </router-link>
+        <router-link @click.native="closeMenu" to="/dmca">
+          <span class="material-icons-outlined">lock</span>
+          <span class="label">DMCA</span>
+        </router-link>
+        <button @click="openVisualSettings" class="menu-button">
           <span class="material-icons">palette</span>
           <span class="label">Vizuálne nastavenia</span>
         </button>
-        <Transition name="fade">
-          <VisualSettingsModal v-if="isVisualMenuOpened" @close="isVisualMenuOpened = false" />
-        </Transition>
       </section>
+      <Transition name="fade">
+        <VisualSettingsModal v-if="isVisualMenuOpened" @close="isVisualMenuOpened = false" />
+      </Transition>
     </main>
     <Footer :style="{ marginTop: 'auto', marginLeft: '10px' }" />
+    <Teleport to="body">
+      <div style="background-color: black; position: fixed; width: 100%; height: 100vh; top: 0; left: 0; opacity: 0.5; z-index: 100;"></div>
+    </Teleport>
   </aside>
 </template>
 
 <script setup lang="ts">
-import NavButton from './Buttons/NavButton.vue'
-import SidebarMenuTile from './Sidebar/SidebarMenuTile.vue'
 import Footer from './Footer.vue'
-import Logo from './Logo.vue'
 import Avatar from './Avatar.vue'
 import VisualSettingsModal from './VisualSettingsModal.vue'
+import BasicButton from './Buttons/BasicButton.vue'
 
 import { notify } from "@kyvg/vue3-notification"
 import { onClickOutside } from '@vueuse/core'
-import { ref, inject } from 'vue'
+import { ref, inject, onMounted, onUnmounted } from 'vue'
 import Auth from '../auth/main'
+import _ from '../utils/main'
 import useEvent from '../composables/event'
-import BasicButton from './Buttons/BasicButton.vue'
 
 const store = inject<any>('store')
 const menu = ref<null | HTMLDivElement>(null)
@@ -62,53 +77,20 @@ const emit = defineEmits(['closeMenu','menu'])
 
 const isVisualMenuOpened = ref(false)
 
-const links = [
-  {
-    icon: `home`,
-    label: 'Domov',
-    isLink: true,
-    route: '/',
-    shown: true
-  },
-  {
-    icon: `video_library`,
-    label: 'Knižnica',
-    isLink: true,
-    route: '/library',
-    shown: true
-  },
-  {
-    icon: `recommend`,
-    label: 'Odporúčané',
-    isLink: true,
-    route: '/recommended',
-    shown: true
-  },
-  {
-    icon: 'admin_panel_settings',
-    label: 'Admin',
-    isLink: true,
-    route: '/admin',
-    shown: store.state.credentials.user?.isAdmin || false
-  },
-  {
-    icon: 'lock',
-    label: 'DMCA',
-    isLink: true,
-    route: '/dmca',
-    shown: true
-  }
-]
-
 const logout = () => Auth.logout().then(res => notify({ type: 'success', text: res.message }))
 
 const closeAtEscapeKeydown = (e: KeyboardEvent) => { if(e.code === 'Escape') closeMenu() }
+
+function openVisualSettings() { isVisualMenuOpened.value = true }
 
 useEvent({ target: document, event: 'keydown', callback: closeAtEscapeKeydown })
 
 onClickOutside(menu, () => closeMenu())
 
 const closeMenu = () => emit('closeMenu', true)
+
+onMounted(() => _.makeUnscrollable(document.body, true))
+onUnmounted(() => _.makeUnscrollable(document.body, false))
 </script>
 
 <style lang="scss" scoped>
@@ -158,8 +140,10 @@ aside{
   }
   section.user{
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
     align-items: center;
+    background-color: var(--card-color);
+    padding: 10px;
     div.user-info{
       div.username{
         display: flex;
@@ -177,69 +161,55 @@ aside{
     flex-direction:column;
     gap:10px;
     overflow-y:scroll;
-    padding:0.75rem 0.5rem 0.5rem 0.75rem;
+    
     overflow-x:hidden;
 
-    &::-webkit-scrollbar{width:15px;height:15px;}
+    &::-webkit-scrollbar{ width:15px; height:15px; }
     &::-webkit-scrollbar-thumb{background:var(--card-color);border:4px solid transparent;border-radius:10px;background-clip:content-box;}
-    section.menu-items{
+    section.menu{
       display:flex;
       flex-direction:column;
       align-items: flex-start;
       gap:0.5rem;
+      padding:0.75rem;
 
-      button{
+      button,a{
         font-size: 0.75rem;
         display: flex;
         align-items: center;
         gap:10px;
-        background-color: var(--card-color);
         width: 100%;
         padding: 8px;
         border-radius: 0.25rem;
+        font-weight: 700;
+
+        span:first-of-type{
+          opacity: 0.5;
+          transition: 0.2s ease opacity;
+        }
 
         span:last-of-type{
           font-size: 0.85rem;
         }
 
+        &.router-link-active{
+          color: var(--theme-color);
+
+          span:first-of-type{
+            opacity: 1;
+          }
+        }
+
         &:hover{
-          background-color: var(--card-color-hover);
-        }
-      }
-    }
-    section.menu-tabs{
-      display:grid;
-      grid-template-columns:repeat(3, 1fr);
-      gap:10px;
-    }
-    div.grid-list-sec{
-      display:grid;
-      grid-template-columns:repeat(3, 1fr);
-      gap:1rem;
-      a.recent-item{
-        background-color:var(--primary-color);
-        overflow:hidden;
-        border-radius:1rem;
-        aspect-ratio:2/3;
-        img{
-          width:100%;
-          height:auto;
-          object-fit:cover;
-        }
-        span{
-          font-size:0.5rem;
-          line-height:1;
-          display:flex;
-          justify-content:center;
-          align-items:center;
-          padding:5px;
-          width:100%;
-          height:100%;
+          background-color: var(--card-color);
+
+          span:first-of-type{
+            opacity: 1;
+          }
         }
       }
     }
   }
-  span.no-history{ font-size:0.8rem }
 }
 @media screen and (max-width: 600px){
   aside{
