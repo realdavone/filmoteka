bace63cefe4612a772fae15185652ce3861cac8b
realdavone <davidkuljovsky@gmail.com>
2023-03-17 13:50:11 +0100

    added google ver metatag

--- frontend/src/components/TV/TVPlayerPanel.vue
+++ frontend/src/components/TV/TVPlayerPanel.vue
@@ -28,7 +28,10 @@
       :season="currentSeasonAndEpisode.season + 1"
       :episode="currentSeasonAndEpisode.episode + 1"
     />
-    <section v-if="(props['lastEpisode'] !== null || props['nextEpisode'] !== null) && (store.state.globalSettings.allowWatchWhileUnregistered || store.state.credentials.loggedIn)" class="episode-card-holder">
+    <section
+      v-if="(props['lastEpisode'] !== null || props['nextEpisode'] !== null) && (store.state.globalSettings.allowWatchWhileUnregistered || store.state.credentials.loggedIn)"
+      class="episode-card-holder"
+    >
       <EpisodeCard 
         v-if="props['lastEpisode']"
         label="Posledná epizóda"
@@ -38,8 +41,9 @@
           episode: props['lastEpisode']['episode_number'],
           date: props['lastEpisode']['air_date'],
         }"
-        @playEpisode="playEpisode" />
-        <EpisodeCard
+        @playEpisode="playEpisode"
+      />
+      <EpisodeCard
         v-if="props['nextEpisode']"
         label="Nasledujúca epizóda"
         :playable="false"
@@ -121,61 +125,6 @@ onBeforeMount(() => {
 </script>
 
 <style lang="scss" scoped>
-div.season-select-holder, div.episode-select-holder{
-  width:100%;
-  --option-height:40px;
-  align-self:flex-start;
-  position:relative;
-  div.selected-season, div.selected-episode, div.option{
-    min-height:var(--option-height);
-    display:flex;
-    gap:8px;
-    align-items:center;
-    padding:0 8px;
-    cursor:pointer;
-    div.poster-holder{
-      height:26px;
-      width:26px;
-      border-radius:4px;
-      overflow:hidden;
-      background:var(--primary-color);
-      img{
-        width:100%;
-        height:auto; 
-      }
-    }
-    span{
-      &.episode-count{
-        font-weight:700;
-      }
-    }
-    div.content-holder{
-      display:flex;
-      align-items:center;
-      flex-direction:column;
-    }
-    &[data-active=true]{
-      background:var(--theme-color);
-      color:var(--font-color-dark);
-    }
-  }
-  div.selected-season, div.selected-episode{ font-weight:700 }
-  div.option:not([data-active=true]):hover{ background:var(--card-color-hover) }
-  div.options{
-    display:flex;
-    flex-direction:column;
-    position:absolute;
-    width:100%;
-    z-index:3;
-    left:0;
-    background:var(--card-color);
-    max-height:200px;
-    overflow:auto;
-    box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
-    border-radius:8px;
-    top:calc(100% + 5px);
-  }
-}
 section.outter-holder{
   display:flex;
   flex-direction:column;
@@ -188,36 +137,6 @@ section.outter-holder{
     gap:var(--container-padding);
   }
   section.player-holder-wrap{ background-color:var(--player-background-color); }
-  div.season-episode{
-    width:100%;
-    display:flex;
-    align-items:center;
-    background:var(--card-color);
-    height:40px;
-    gap:5px;
-    padding:0 5px;
-    border-radius: 25px;
-    button{
-      color:var(--theme-color);
-      display:flex;
-      justify-content:center;
-      align-items:center;
-      transition:0.2s ease background;
-      border-radius:50%;
-      min-width:28px!important;
-      max-width:28px!important;
-      width:28px;
-      height:28px;
-      &.next-previous-episode{
-        padding-bottom:6px;
-        font-size: 40px!important;
-      }
-      &.next-previous-episode.disabled{
-        cursor:default;
-        color:gray;
-      }
-    }
-  }
 }
 @media screen and (max-width: 1300px) {
   section.outter-holder{
